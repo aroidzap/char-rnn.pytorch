7585ac6b745600da1dbd73122fed3e64e230455f
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2024-11-24 20:26:49 +0100

    update corpus processing

--- ccv_corpus_export.py
+++ ccv_corpus_export.py
@@ -31,7 +31,7 @@ def process_corpus(args, corpus, num_all_poems, append = False):
             t = time.time()
         if args.p_author_identity is None or item["p_author"]["identity"].strip().lower() == args.p_author_identity.strip().lower():
             num_filtered_poems += 1
-            filtered_corpus_text += get_poem(item, include_poem_name=args.with_poem_names)
+            filtered_corpus_text += get_poem(item, include_poem_name=args.with_poem_name)
     print(f"Found {num_all_poems + num_filtered_poems} poems{extra_info}.")
     
     if len(filtered_corpus_text) or not append:
@@ -50,14 +50,29 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--p_author_identity', default=None, help="Erben, Karel Jaromír")
-    parser.add_argument('--with_poem_names', action='store_true')
-    parser.add_argument('--ccv_path', default="submodules/corpusCzechVerse/ccv")
+    parser.add_argument('--with-poem-names', action='store_true')
+    parser.add_argument('--ccv-download-folder', default="data")
     parser.add_argument('--output', default="data/export.txt")
     args = parser.parse_args()
 
+    # Download corpusCzechVerse
+    if not os.path.exists(args.ccv_download_folder):
+        
+        from io import BytesIO
+        from zipfile import ZipFile
+        import requests
+
+        ccv_url = "https://github.com/versotym/corpusCzechVerse/archive/refs/heads/master.zip"
+        print(f"Downloading {ccv_url}")
+        ccv_zip = ZipFile(BytesIO(requests.get(ccv_url).content))
+        print(f"Extracting to {args.ccv_download_folder}")
+        if not os.path.exists(os.path.dirname(args.ccv_download_folder)):
+            os.makedirs(os.path.dirname(args.ccv_download_folder))
+        ccv_zip.extractall(args.ccv_download_folder, strip=True)
+
     # Load and process whole corpus
     num_all_poems = 0
-    files=list(pathlib.Path(args.ccv_path).rglob("*.json"))
+    files=list(pathlib.Path(f"{args.ccv_download_folder}/corpusCzechVerse-master/ccv").rglob("*.json"))
     for i, filename in enumerate(files):
         print(f"Loading corpus {i+1}/{len(files)}.")
         with open(filename) as file:
---
+++ prepare_corpus.py
@@ -0,0 +1,80 @@
+import poetree
+import tqdm
+
+# see https://github.com/versotym/poetree
+
+def process_poem(args, poem):
+    poem_text = ""
+    paragraph_id = None
+
+    for line in poem.get_body():
+        poem_text += line['text'] + "\n"
+
+        # print extra newline after paragraph
+        if paragraph_id != line['id_stanza'] and paragraph_id is not None:
+            poem_text += "\n"
+        paragraph_id = line['id_stanza']
+
+    if args.without_poem_names:
+        text = f"{poem_text}\n"
+    else:
+        text = f"{poem.title}\n{poem_text}\n"
+    return text
+
+def process_corpus(args):
+
+    corpus = poetree.Corpus(args.lang)
+
+    if len(args.filter_author):
+        # filter only selected authors
+        n_poems = 0
+        authors = []
+        for author in corpus.get_authors():
+            if author.name in args.filter_author:
+                n_poems += author.n_poems
+                authors += [author]
+    else:
+        n_poems = corpus.n_poems
+        authors = corpus.get_authors()
+
+    with tqdm.tqdm(total=n_poems) as progress_bar:
+        for author in authors:
+
+            for poem in author.get_poems():
+                progress_bar.set_description(f"{author.name}; {poem.title}")
+
+                text = process_poem(args, poem)
+
+                if args.with_author_names:
+                    text = f"{author.name} ({author.born})\n\n" + text
+                yield text
+
+                progress_bar.update(1)
+
+def list_authors(args):
+    corpus = poetree.Corpus(args.lang)
+    for author in corpus.get_authors():
+        author_txt = f"{author.name} ({author.born}-{author.died})"
+        n_poems_txt = f"Poems: {author.n_poems}"
+        cmd_txt = f'--filter-author "{author.name}"'
+        print(f"{n_poems_txt:<15}{author_txt:<60}{cmd_txt:<60}")
+                
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', default="data/export.txt")
+    parser.add_argument('--lang', default='cs', help="Corups language")
+    parser.add_argument('--filter-author', nargs='+', default=[], help="Limit to only some authors")
+    parser.add_argument('--list-authors', action='store_true')
+    parser.add_argument('--without-poem-name', action='store_true')
+    parser.add_argument('--with-author-name', action='store_true')
+    args = parser.parse_args()
+
+    if args.list_authors:
+        list_authors(args)
+    else:
+        with open(args.output, "w", encoding="utf-8") as out:
+            for part in process_corpus(args):
+                out.write(part)
+
